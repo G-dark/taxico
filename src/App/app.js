b7c5127a82e7ed5ba91f15f93ea124ea5ee488db
Edison Pacheco <80083216+G-dark@users.noreply.github.com>
2025-01-21 01:42:32 -0500

    Update app.js

--- src/App/app.js
+++ src/App/app.js
@@ -227,10 +227,7 @@ app.use(repair);
 app.use(taxiDriver);
 app.use(car);
 app.use(carFee);
-fetch('https://ifconfig.me')
-  .then(response => response.text())
-  .then(ip => console.log(`La IP pública de salida es: ${ip}`))
-  .catch(err => console.error('Error al obtener la IP:', err));
+
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`escuchando en el puerto, ${PORT}`);
 });
